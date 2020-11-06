d5beab7c4ac1942a51811676932872d30f872fd7
Svetlana-Orlova <Svetlana-Orlova@github.com>
2020-11-06 20:15:24 +0300

    Добавлено перемещение главного пина

---
+++ js/mainPin.js
@@ -0,0 +1,99 @@
+
+'use strict';
+(function () {
+  const MAIN_PIN_POINTER_HEIGHT = 10;
+  const mapElement = document.querySelector(`.map`);
+  const mainPinElement = mapElement.querySelector(`.map__pin--main`);
+  const advertFormElement = document.querySelector(`.ad-form`);
+  const addressInputElement = advertFormElement.querySelector(`#address`);
+  const mainPinHeight = mainPinElement.offsetHeight + MAIN_PIN_POINTER_HEIGHT;
+  const mainPinHalfWidth = Math.floor(mainPinElement.offsetWidth / 2);
+
+  const CoordinateX = {
+    MIN: 0,
+    MAX: mapElement.offsetWidth
+  };
+
+  const CoordinateY = {
+    MIN: 130,
+    MAX: 630
+  };
+
+  const MainPinPositionX = {
+    MIN: CoordinateX.MIN - mainPinHalfWidth,
+    MAX: CoordinateX.MAX - mainPinHalfWidth
+  };
+  const MainPinPositionY = {
+    MIN: CoordinateY.MIN - mainPinHeight,
+    MAX: CoordinateY.MAX - mainPinHeight
+  };
+
+  mainPinElement.addEventListener(`mousedown`, function (evt) {
+    evt.preventDefault();
+
+    let StartCoords = {
+      X: evt.clientX,
+      Y: evt.clientY
+    };
+
+    function onMouseMove(moveEvt) {
+      moveEvt.preventDefault();
+
+      const Shift = {
+        X: StartCoords.X - moveEvt.clientX,
+        Y: StartCoords.Y - moveEvt.clientY
+      };
+
+      StartCoords = {
+        X: moveEvt.clientX,
+        Y: moveEvt.clientY
+      };
+
+      let mainPinY = mainPinElement.offsetTop - Shift.Y;
+      let mainPinX = mainPinElement.offsetLeft - Shift.X;
+
+      if (mainPinY <= MainPinPositionY.MIN) {
+        mainPinY = MainPinPositionY.MIN;
+      } else if (mainPinY >= MainPinPositionY.MAX) {
+        mainPinY = MainPinPositionY.MAX;
+      }
+
+      if (mainPinX <= MainPinPositionX.MIN) {
+        mainPinX = MainPinPositionX.MIN;
+      } else if (mainPinX >= MainPinPositionX.MAX) {
+        mainPinX = MainPinPositionX.MAX;
+      }
+
+      mainPinElement.style.top = `${mainPinY}px`;
+      mainPinElement.style.left = `${mainPinX}px`;
+
+      addressInputElement.value = `${mainPinX + mainPinHalfWidth}, ${mainPinY + mainPinHeight}`;
+    }
+
+    function onMouseUp(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener(`mousemove`, onMouseMove);
+      document.removeEventListener(`mouseup`, onMouseUp);
+    }
+
+    document.addEventListener(`mousemove`, onMouseMove);
+    document.addEventListener(`mouseup`, onMouseUp);
+  });
+
+  function fillAddress() {
+    const addressX = Math.round(parseInt(mainPinElement.style.left, 10) + mainPinElement.clientWidth / 2);
+    let addressY;
+    if (advertFormElement.classList.contains(`ad-form--disabled`)) {
+      addressY = (Math.round(parseInt(mainPinElement.style.top, 10) + mainPinElement.clientHeight / 2));
+    } else {
+      addressY = (Math.round(parseInt(mainPinElement.style.top, 10) + mainPinHeight));
+    }
+
+    addressInputElement.value = `${addressX}, ${addressY}`;
+  }
+
+  window.mainPin = {
+    fillAddress
+  };
+})();
--- js/map.js
+++ js/map.js
@@ -1,14 +1,11 @@
 'use strict';
 
 (function () {
-  const PIN_WIDTH = 50;
-  const PIN_HEIGHT = 70;
   const PRIMARY_MOUSE_BUTTON = 0;
   const mapElement = document.querySelector(`.map`);
   const mainPinElement = mapElement.querySelector(`.map__pin--main`);
   const fieldsetElements = document.querySelectorAll(`fieldset`);
   const advertFormElement = document.querySelector(`.ad-form`);
-  const addressInputElement = advertFormElement.querySelector(`#address`);
 
   function disableItems(items) {
     for (let i = 0; i < items.length; i++) {
@@ -24,21 +21,11 @@
     advertFormElement.classList.remove(`ad-form--disabled`);
   }
 
-  function fillAddress(item) {
-    const addressX = item.style.left;
-    const addressY = item.style.top;
-    if (advertFormElement.classList.contains(`ad-form--disabled`)) {
-      addressInputElement.value = `${parseInt(addressX, 10) + PIN_WIDTH / 2 }, ${parseInt(addressY, 10) + PIN_HEIGHT / 2}`;
-    } else {
-      addressInputElement.value = `${parseInt(addressX, 10) + PIN_WIDTH}, ${parseInt(addressY, 10) + PIN_HEIGHT}`;
-    }
-  }
-
   function deactivatePage() {
     mapElement.classList.add(`map--faded`);
     advertFormElement.classList.add(`ad-form--disabled`);
     disableItems(fieldsetElements);
-    fillAddress(mainPinElement);
+    window.mainPin.fillAddress(mainPinElement);
 
     mainPinElement.addEventListener(`mousedown`, onPageActivate);
     mainPinElement.addEventListener(`keydown`, onPageActivate);
@@ -47,7 +34,7 @@
   function onPageActivate(evt) {
     if (evt.button === PRIMARY_MOUSE_BUTTON || evt.key === `Enter`) {
       enableItems(fieldsetElements);
-      fillAddress(mainPinElement);
+      window.mainPin.fillAddress(mainPinElement);
       window.pin.insertPins();
 
       mainPinElement.removeEventListener(`mousedown`, onPageActivate);
